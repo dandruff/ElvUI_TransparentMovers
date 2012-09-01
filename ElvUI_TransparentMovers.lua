6c2fefc77e733e521e603ebf3399aa8a4a7e33af
Cimplex <reubend@formetco.com>
2012-09-01 01:17:13 -0500

    Fixing install again

--- ElvUI_TransparentMovers.lua
+++ ElvUI_TransparentMovers.lua
@@ -1,23 +1,23 @@
+-- =======================================================================================
+-- Add-On Name: ElvUI Transparent Movers
+--     License: MIT
+--      Author: Dandruff @ Whisperwind
+-- Description: Allows the user to give all movers a transparency.
+-- =======================================================================================
 
--- Alpha is set 0 - 1; 0.7 = 70%
 local ADDON_NAME, ETM = ...
 
 if not ElvUI then return end
-local E, L, V, P = select(1, unpack(ElvUI))
-local old = E.CreateMover
+local E, L, V, P = unpack(ElvUI)
 
 -- Install
-if not E.db.movers then
-  E.db.movers = { }
-end
-if not E.db.movers.alpha then
-  E.db.movers.alpha = 1
-end
+P["movers"] = {
+  ["alpha"] = 0.5 
+}
 
-E.CreateMover = function(e, p, ...)
-    old(e, p, ...)
-    p.mover:SetAlpha(E.db.movers.alpha)
-  end
+hooksecurefunc(E, 'CreateMover', function(self,p)
+  p.mover:SetAlpha(E.db.movers.alpha)
+end)
 
 E.Options.args.general.args["movers"] = {
   order = 10,
@@ -46,3 +46,24 @@ E.Options.args.general.args["movers"] = {
     },
   },
 }
+
+
+--[==[
+The MIT License (MIT)
+Copyright (c) 2012 Reuben DeLeon
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]==]
